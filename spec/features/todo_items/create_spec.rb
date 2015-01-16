aa477ec413692e1539f441eefe8f7f74abd5d83a
Obi Anaedozie <dozie22@gmail.com>
2015-01-15 22:13:55 -0500

    Add specs and code for editing todo items

--- spec/features/todo_items/create_spec.rb
+++ spec/features/todo_items/create_spec.rb
@@ -16,9 +16,29 @@ describe "Adding todo items" do
       click_link "New Todo Item"
       fill_in "Content", with: "Milk"
       click_button "Save"
-      expect(page).to have_content("Milk")
+      expect(page).to have_content("Added todo list item.")
       within("ul.todo_items") do
         expect(page).to have_content("Milk")
       end
     end
+    it "displays an error with no content" do
+      visit_todo_list(todo_list)
+      click_link "New Todo Item"
+      fill_in "Content", with: ""
+      click_button "Save"
+      within("div.flash") do
+         expect(page).to have_content("There was a problem adding that todo list item.")
+      end
+      expect(page).to have_content("Content can't be blank ")
+     end 
+     it "displays an error with content less than 2 characters long" do
+      visit_todo_list(todo_list)
+      click_link "New Todo Item"
+      fill_in "Content", with: "A"
+      click_button "Save"
+      within("div.flash") do
+         expect(page).to have_content("There was a problem adding that todo list item.")
+      end
+      expect(page).to have_content("Content is too short")
+     end 
 end      
