daacd46c2ce58d9f3c779e4592624fb80d3e2a9d
Obi Anaedozie <dozie22@gmail.com>
2015-01-18 21:07:12 -0500

    Extract todo list helper

--- spec/features/todo_items/index_spec.rb
+++ spec/features/todo_items/index_spec.rb
@@ -4,12 +4,12 @@ describe  "Viewing todo items" do
   let!(:todo_list) { TodoList.create(title: "Grocery list", description: "Groceries")}
        
 
-       def visit_todo_list(list)
-          visit "/todo_lists"
-          within "#todo_list_#{todo_list.id}" do
-          click_link "List Items"
-       end
-      end
+      #  def visit_todo_list(list)
+      #     visit "/todo_lists"
+      #     within "#todo_list_#{todo_list.id}" do
+      #     click_link "List Items"
+      #  end
+      # end
 
        before do
        end
